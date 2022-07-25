ee622304c65127ff46ef39f8a1673c2ab7586e03
Edwin William <edwinwilliam908@gmail.com>
2022-07-24 23:55:57 -0400

    add delete button, input population, and general functionality work.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,53 @@ function App() {
     const [tasks, setTasks] = React.useState([])
     const [task, setTask] = React.useState("")
 
+    function handleSubmit (e) {
+        e.preventDefault()
+
+        const newTask ={
+            id: new Date().getTime(),
+            text: task,
+            completed: false,
+        }
+
+        setTasks([...tasks].concat(newTask))
+        setTask("")
+    }
+
+    function deleteTask(id) {
+        const updatedTasks = [...tasks].filter((task) => task.id !== id )
+
+        setTasks(updatedTasks)
+    }
+
+
+    function toggleComplete(id) {
+        const updatedTasks =[...tasks].map((task) => {
+            if (task.id === id) {
+                task.completed =!task.completed
+            }
+            return task
+        })
+
+        setTasks(updatedTasks)
+    }
+
     return (
         <div className="App">
-            <form>
-                <input type="text" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={(e) => setTask(e.target.value)} value={task}/>
                 <button type="submit">Commit</button>
             </form>
+            {tasks.map((task) => <div key={task.id}>
+                <div>{task.text}</div>
+                <button onClick={() => deleteTask(task.id)}>Delete</button>
+                <input 
+                    type="checkbox" 
+                    onChange={() => toggleComplete(task.id)} 
+                    checked={task.completed} />
+             </div>)}
         </div>
     );
-} 
+}
 
 export default App;
